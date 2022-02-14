e572c9bc3f5d00dbf7899bb8f34b352b11ebd52c
Stephen Ostapenko <stepanos2002@gmail.com>
2022-02-14 22:30:55 +0500

    first version

--- src/main/kotlin/com/flaax/ideolog/main.kt
+++ src/main/kotlin/com/flaax/ideolog/main.kt
@@ -1,5 +1,28 @@
 package com.flaax.ideolog
 
-fun main(args: Array<String>) {
+import java.io.File
 
+fun processLog(logLines: List<String>): List<String> {
+    val dict = mutableMapOf<String, Int>()
+    logLines.forEach { line ->
+        val tokens = line.split("\\s+".toRegex())
+        val threadName = tokens[5]
+        val cnt = dict.getOrDefault(threadName, 0) + 1
+        dict[threadName] = cnt
+    }
+
+    return dict.toList().sortedByDescending { it.second }.take(10).map { "${it.first} ${it.second}" }
+}
+
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("missing file name")
+        return
+    }
+
+    val logLines = File(args[0]).readLines()
+    val mostFreqThreads = processLog(logLines)
+    mostFreqThreads.forEach {
+        println(it)
+    }
 }
