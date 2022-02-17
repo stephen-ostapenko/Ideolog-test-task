e01805d528d11d0d55faf62612f02f239ff62f61
Stephen Ostapenko <stepanos2002@gmail.com>
2022-02-17 14:33:43 +0300

    minor fixes

--- src/main/kotlin/com/flaax/ideolog/main.kt
+++ src/main/kotlin/com/flaax/ideolog/main.kt
@@ -2,17 +2,21 @@ package com.flaax.ideolog
 
 import java.io.File
 
+const val TOP_LINES_NUMBER = 10
+
 fun processLog(logLines: List<String>): List<String> {
+    // map with <name of thread, number of strings>
     val dict = mutableMapOf<String, Int>()
     logLines.forEach { line ->
         val tokens = line.split("\\s+".toRegex())
+        // It looks like thread name is the sixth token in each string
         val threadName = tokens[5]
         val cnt = dict.getOrDefault(threadName, 0) + 1
         dict[threadName] = cnt
     }
 
     val comp = compareByDescending<Pair<String, Int>> { it.second }.thenBy { it.first }
-    return dict.toList().sortedWith(comp).take(10).map { "${it.first} ${it.second}" }
+    return dict.toList().sortedWith(comp).take(TOP_LINES_NUMBER).map { "${it.first} ${it.second}" }
 }
 
 fun main(args: Array<String>) {
